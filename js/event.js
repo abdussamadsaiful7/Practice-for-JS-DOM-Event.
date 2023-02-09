00949a96e46b23c2e627fa7d8cc6d5cf3c7b6ced
abdussamadsaiful7 <samadsaifullah11@gmail.com>
2023-02-10 01:16:30 +0600

    add goldenrod color button

--- js/event.js
+++ js/event.js
@@ -11,13 +11,12 @@ function makeRed(){
 
 
 //option-3 
-
 const makeBlueButton= document.getElementById('make-blue');
         makeBlueButton.onclick = makeBlue;
-
         function makeBlue(){
             document.body.style.backgroundColor = 'blue';
         };
+
 
 //option-3, another 
     const purpleButton = document.getElementById('make-purple');
@@ -25,14 +24,23 @@ const makeBlueButton= document.getElementById('make-blue');
             document.body.style.backgroundColor = 'purple';
         };
 
+
 //option-4, 
-
     const pinkButton = document.getElementById('make-pink');
-        pinkButton.addEventListener('click', makePink);
-
-        function makePink(){
+    pinkButton.addEventListener('click', makePink);
+    function makePink(){
             document.body.style.backgroundColor = 'pink';
         };
 
-//option-4, another 
 
+//option-4, another 
+const makeGreenButton = document.getElementById('make-green');
+        makeGreenButton.addEventListener('click', function makeGreen(){
+            document.body.style.backgroundColor='green';
+        });
+
+
+//option-4, final one
+document.getElementById('make-goldenrod').addEventListener('click', function(){
+    document.body.style.backgroundColor = 'goldenrod';
+ });
