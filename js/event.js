0ef0ab08713368fc9fe3c37901a35f4a9f1000e2
abdussamadsaiful7 <samadsaifullah11@gmail.com>
2023-02-10 00:21:14 +0600

    add purple color button

--- js/event.js
+++ js/event.js
@@ -1,6 +1,22 @@
 console.log('This is separate file');
 
-//add onclick function.
+//option-2, add onclick function.
 function makeRed(){
     document.body.style.backgroundColor='red';
 }
+
+
+//option-3 
+
+const makeBlueButton= document.getElementById('make-blue');
+        makeBlueButton.onclick = makeBlue;
+
+        function makeBlue(){
+            document.body.style.backgroundColor = 'blue';
+        };
+
+//option-3, another 
+        const purpleButton = document.getElementById('make-purple');
+        purpleButton.onclick = function makePurpleButton(){
+            document.body.style.backgroundColor = 'purple';
+        }
